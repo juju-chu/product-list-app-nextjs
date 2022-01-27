73242592ca8c3ef8a2868f7840e17514fa445335
Juju <jujuchu@pixnet.tw>
2022-01-27 22:43:21 +0800

    feat: 使用價格排序產品列表

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,13 +1,27 @@
-import { getAllProduct } from "../../fake-data";
+import { useState, ChangeEvent } from "react";
+import { sortByPrice, Direction } from "../../fake-data";
 import ProductCard from "../../components/ProductCard";
-import { PageTitle, ProductGallery } from "./index.style";
+import { PageTitle, ProductGallery, PriceFilter } from "./index.style";
 
 const Home = () => {
-    const products = getAllProduct();
+    const [direction, setDirection] = useState<Direction>("ASC");
+
+    const products = sortByPrice(direction);
+
+    const handleSortingDirectionChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setDirection(e.target.value as Direction);
+    };
 
     return (
         <>
             <PageTitle>商品列表</PageTitle>
+            <PriceFilter>
+                Price:
+                <select value={direction} onChange={handleSortingDirectionChange}>
+                    <option value="ASC">價格由低到高</option>
+                    <option value="DES">價格由高到低</option>
+                </select>
+            </PriceFilter>
             <ProductGallery>
                 {products.map((product) => (
                     <ProductCard key={product.id} product={product} />
